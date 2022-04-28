c6b4276346c33d02a4cc3549a0092784027cf0ba
Mohsn <mohsenmohseni.co@gmail.com>
2022-04-28 09:35:29 +0430

    test sync and async mode

--- next-steps/myapp.py
+++ next-steps/myapp.py
@@ -7,3 +7,22 @@ print(add_result.get())
 
 divide_result = divide.delay(4, 2)
 print(divide_result.get())
+
+
+# async mode
+add_result1 = add.apply_async(args=[4, 2], countdown=15)
+add_result1.forget()
+
+
+divide_result1 = add.apply_async(args=[999, 3], countdown=5)
+divide_result1.forget()
+
+
+# sync mode
+add_result1 = add.apply_async(args=[4, 2], countdown=15)
+add_result1.get()
+
+
+divide_result1 = add.apply_async(args=[999, 3], countdown=5)
+divide_result1.get()
+
