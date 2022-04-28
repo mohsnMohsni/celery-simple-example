482b1778c7b7fca9c949d7857cbdb3e6c20f65e3
Mohsn <mohsenmohseni.co@gmail.com>
2022-04-28 09:52:34 +0430

    define queue

--- next-steps/myapp2.py
+++ next-steps/myapp2.py
@@ -1,3 +1,4 @@
+import queue
 from proj.tasks import add
 from proj.celery import app
 
@@ -9,7 +10,8 @@ res = app.AsyncResult(add_result_id)
 print(res.state)
 
 
-add_result2 = add.apply_async(args=[4, 2])
+add_result2 = add.apply_async(args=[4, 2], queue='celery') # default queue is celery and it's create by celery, worker lissten to this queue
+                                                            # for define new queue we should define and create new queue on celery run syntax   
 add_result2_id = add_result2.id
 
 res = app.AsyncResult(add_result2_id)
