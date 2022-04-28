37ff8bd7404bb5e10349fa16416c0b48787fd5c7
Mohsn <mohsenmohseni.co@gmail.com>
2022-04-28 08:48:47 +0430

    config proj dir to celery

---
+++ next-steps/proj/celeryconfig.py
@@ -0,0 +1,13 @@
+from xml.etree.ElementInclude import include
+
+
+broker_url = 'redis://localhost:6379'
+result_backend = 'redis://localhost:6379'
+
+include = ['proj.tasks',]
+
+task_serializer = 'json'
+result_serializer = 'json'
+accept_content = ['json']
+timezone = 'Asia/Tehran'
+enable_utc = True
