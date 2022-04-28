57c4708647b01d96d1e328d4e23ae56189807d4e
Mohsn <mohsenmohseni.co@gmail.com>
2022-04-28 08:52:04 +0430

    config celery app and start app

--- next-steps/proj/celery.py
+++ next-steps/proj/celery.py
@@ -1,5 +1,11 @@
+from __future__ import absolute_import, unicode_literals
+
 from celery import Celery
 
 
 app = Celery('proj')
 app.config_from_object('celeryconfig')
+
+
+if __name__ == '__main__':
+    app.start()
