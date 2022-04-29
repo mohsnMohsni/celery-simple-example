4d61ea36ecc57f24ace0de20cf5a28ead0705975
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2022-04-29 22:42:25 +0430

    add print

--- user-guide/proj2/tasks.py
+++ user-guide/proj2/tasks.py
@@ -7,12 +7,14 @@ from proj2.celery import app
 def decorator1(func):
     @wraps(func)  # do like pure handle meta data in bottom code
     def wrapper(*args, **kwargs):
+        print('decorator 1')
         return func(*args, **kwargs)
     return wrapper
 
 
 def decorator2(func):
     def wrapper(*args, **kwargs):
+        print('decorator 2')
         return func(*args, **kwargs)
     # pass meta data of function to wrapper, because celery use meta data to declear address of func.
     wrapper.__doc__ = func.__doc__
