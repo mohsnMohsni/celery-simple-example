26ba2b92a576428345f2990505566700c6c1cdbe
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2022-04-29 09:59:56 +0430

    add task route config for use different queue

--- next-steps/proj/celeryconfig.py
+++ next-steps/proj/celeryconfig.py
@@ -11,3 +11,11 @@ result_serializer = 'json'
 accept_content = ['json']
 timezone = 'Asia/Tehran'
 enable_utc = True
+
+
+# command shell: celery -A appName worker -Q celery,queue1,queue2,queue3 -l info
+task_routes = {
+    'proj.tasks.add': {'queue': 'queue1'}, # name of queue pass in command shell
+    'proj.tasks.divide': {'queue': 'queue2'},
+    'proj.tasks.average': {'queue': 'queue3'},
+}
