9977dafc0e7985f6618b5bbd273d6260092bd0c9
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2022-04-29 19:08:18 +0430

    test parallel takss

--- user-guide/myapp.py
+++ user-guide/myapp.py
@@ -1,4 +1,17 @@
-from proj2.tasks import say_hello_world
+import time
+from timeit import timeit
+
+from proj2.tasks import say_hello_world, run_in_async_mode
 
 
 say_hello_world.delay()
+
+
+# use async mode to handle to work parallel
+def check_():
+    async_res = run_in_async_mode.delay()
+    time.sleep(3)
+    async_res.get()
+
+
+print(timeit(check_, number=1))
--- user-guide/proj2/tasks.py
+++ user-guide/proj2/tasks.py
@@ -1,6 +1,14 @@
+import time
+
 from proj2.celery import app
 
 
 @app.task
 def say_hello_world():
     return 'Hello world.'
+
+
+@app.task
+def run_in_async_mode():
+    time.sleep(5)
+    return 'Done.'
