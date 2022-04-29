6bebd9ffbbbba1c881d36edbcf48ec70ed632823
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2022-04-29 22:41:31 +0430

    use decorator with task decorator and handle meta data

--- user-guide/proj2/tasks.py
+++ user-guide/proj2/tasks.py
@@ -1,9 +1,28 @@
 import time
+from functools import wraps
 
 from proj2.celery import app
 
 
+def decorator1(func):
+    @wraps(func)  # do like pure handle meta data in bottom code
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def decorator2(func):
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    # pass meta data of function to wrapper, because celery use meta data to declear address of func.
+    wrapper.__doc__ = func.__doc__
+    wrapper.__name__ = func.__name__
+    return wrapper
+
+
 @app.task
+@decorator1
+@decorator2
 def say_hello_world():
     return 'Hello world.'
 
